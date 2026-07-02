23dfab4a10bdd7a91c627b82c9b94c88387e6f25
HakanHakan1 <149481420+HakanHakan1@users.noreply.github.com>
2026-07-02 12:52:47 +0200

    newer hailo interface in runtime.py

--- orchestration/hailo/runtime.py
+++ orchestration/hailo/runtime.py
@@ -6,56 +6,36 @@ class HailoModel:
         self.hef_path = str(hef_path)
 
     def __enter__(self):
-        from hailo_platform import (  # type: ignore
-            ConfigureParams,
-            FormatType,
-            HailoStreamInterface,
-            HEF,
-            InferVStreams,
-            InputVStreamParams,
-            OutputVStreamParams,
-            VDevice,
-        )
+        from hailo_platform import FormatType, HEF, VDevice  # type: ignore
 
-        self.hef = HEF(self.hef_path)
+        hef = HEF(self.hef_path)
         self.device = VDevice()
-        self.target = self.device.__enter__()
-        params = ConfigureParams.create_from_hef(
-            self.hef,
-            interface=HailoStreamInterface.PCIe,
-        )
-        self.network_group = self.target.configure(self.hef, params)[0]
-        self.network_params = self.network_group.create_params()
+        self.device.__enter__()
 
-        input_info = self.hef.get_input_vstream_infos()[0]
-        output_info = self.hef.get_output_vstream_infos()[0]
+        input_info = hef.get_input_vstream_infos()[0]
+        output_info = hef.get_output_vstream_infos()[0]
         self.input_name = input_info.name
         self.output_name = output_info.name
 
-        input_params = InputVStreamParams.make(
-            self.network_group,
-            format_type=FormatType.FLOAT32,
-        )
-        output_params = OutputVStreamParams.make(
-            self.network_group,
-            format_type=FormatType.FLOAT32,
-        )
+        self.model = self.device.create_infer_model(self.hef_path)
+        self.model.input().set_format_type(FormatType.FLOAT32)
+        self.model.output().set_format_type(FormatType.FLOAT32)
 
-        self.activation = self.network_group.activate(self.network_params)
-        self.activation.__enter__()
-        self.streams = InferVStreams(
-            self.network_group,
-            input_params,
-            output_params,
-        )
-        self.pipeline = self.streams.__enter__()
+        self.configured_model = self.model.configure()
+        self.configured_model.__enter__()
         return self
 
     def __exit__(self, exc_type, exc, traceback):
-        self.streams.__exit__(exc_type, exc, traceback)
-        self.activation.__exit__(exc_type, exc, traceback)
+        self.configured_model.__exit__(exc_type, exc, traceback)
         self.device.__exit__(exc_type, exc, traceback)
 
     def infer(self, batch):
-        output = self.pipeline.infer({self.input_name: batch.astype(np.float32)})
-        return np.asarray(output[self.output_name])
+        bindings = self.configured_model.create_bindings()
+        bindings.input().set_buffer(batch.astype(np.float32))
+
+        output_shape = tuple(self.model.output().shape)
+        output = np.empty(output_shape, dtype=np.float32)
+        bindings.output().set_buffer(output)
+
+        self.configured_model.run([bindings], timeout_ms=10000)
+        return output
